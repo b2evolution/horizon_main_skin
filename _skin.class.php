0643c872fbf1ea3c16bb3b4f5d413e3485516fcc
Francois Planque <github@fplanque.net>
2018-08-30 21:10:18 +0200

    doc

--- _skin.class.php
+++ _skin.class.php
@@ -94,7 +94,7 @@ class horizon_main_Skin extends Skin
 	 * This should NOT be protected. It should be used INSTEAD of file parsing.
 	 * File parsing should only be used if this function is not defined
 	 *
-	 * @return array
+	 * @return array Array which overrides default containers; Empty array means to use all default containers.
 	 */
 	function get_declared_containers()
 	{
